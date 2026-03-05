785eda36a7c56c59e3053dee212bd6e7e82d6af3
renhirose <ren_hirose28@milton.edu>
2026-03-05 14:52:40 -0500

    added objects as Arrays

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,5 +1,6 @@
 public class Arrays {
     public int[]numbs;
+    public Astronaut[]  astro;
     public static void main(String[] args) {
         System.out.println("welcome to awesome arrays");
         new Arrays();
@@ -8,19 +9,30 @@ public class Arrays {
     }
     public Arrays(){
 
+
+
+
         numbs=new int[100];
+        Astronaut A1 =new Astronaut();
+        astro=new Astronaut[7];
+        astro[4]=new Astronaut();
 
         for(int y=0;y<numbs.length; y++){
             numbs[y]=(int)(Math.random()*1834)+1;
 
 
         }
+        astro[4].eyes=12;
+        astro[4].name="gerald";
+        System.out.println(astro[4].name);
 
         displayArray();
         summary();
         averageNum();
         MaxNum();
         MinNum();
+
+        A1.primtInfo();
 
 
 
@@ -43,6 +55,8 @@ public class Arrays {
 
     public void averageNum(){
         System.out.println(java.util.Arrays.stream(numbs).average());
+
+
 
 
     }
---
+++ src/Astronaut.java
@@ -0,0 +1,17 @@
+public class Astronaut {
+    public String name;
+    public int eyes;
+    public Astronaut(){
+        name="stephen b";
+        eyes=13;
+
+
+
+    }
+   public void primtInfo(){
+
+       System.out.println("the astronaut has "+eyes+" eyes");
+       System.out.println("the astronaut's name is "+name);
+   }
+
+}
