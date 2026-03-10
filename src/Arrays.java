8571e15dae56f4a2c5b105d23e149e66691b4e18
renhirose <ren_hirose28@milton.edu>
2026-03-09 20:50:36 -0400

    added all the astro object arrays

--- src/Arrays.java
+++ src/Arrays.java
@@ -15,16 +15,62 @@ public class Arrays {
         numbs=new int[100];
         Astronaut A1 =new Astronaut();
         astro=new Astronaut[7];
-        astro[4]=new Astronaut();
+
 
         for(int y=0;y<numbs.length; y++){
             numbs[y]=(int)(Math.random()*1834)+1;
 
 
         }
-        astro[4].eyes=12;
-        astro[4].name="gerald";
-        System.out.println(astro[4].name);
+        astro[0] = new Astronaut();
+
+        astro[0].eyes = 2;
+
+        astro[0].name = "ben";
+        astro[0].primtInfo();
+
+        astro[1] = new Astronaut();
+
+        astro[1].eyes = 13;
+
+        astro[1].name = "mike";
+        astro[1].primtInfo();
+
+        astro[2] = new Astronaut();
+
+        astro[2].eyes = 24;
+
+        astro[2].name = "stephen b";
+        astro[2].primtInfo();
+
+        astro[3] = new Astronaut();
+
+        astro[3].eyes = 8;
+
+        astro[3].name = "joseph";
+        astro[3].primtInfo();
+
+        astro[4] = new Astronaut();
+
+        astro[4].eyes = 10;
+
+        astro[4].name = "jacob";
+        astro[4].primtInfo();
+        astro[5] = new Astronaut();
+
+        astro[5].eyes = 20;
+
+        astro[5].name = "gerald";
+        astro[5].primtInfo();
+        astro[6] = new Astronaut();
+
+        astro[6].eyes = 18;
+
+        astro[6].name = "naomi";
+        astro[6].primtInfo();
+
+
+
 
         displayArray();
         summary();
